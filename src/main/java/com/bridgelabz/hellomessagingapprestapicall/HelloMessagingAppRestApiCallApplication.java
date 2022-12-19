74a1eb2acd4c3c6461ebbf0b9e5668e2f24e3331
nitis <nitish8152@gmail.com>
2022-12-19 19:28:54 +0530

    Added:Welcome To Hello Messaging App

---
+++ src/main/java/com/bridgelabz/hellomessagingapprestapicall/HelloMessagingAppRestApiCallApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.hellomessagingapprestapicall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMessagingAppRestApiCallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMessagingAppRestApiCallApplication.class, args);
+        System.out.println("Welcome to Hello Messaging App");
+
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/hellomessagingapprestapicall/HelloMessagingAppRestApiCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellomessagingapprestapicall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessagingAppRestApiCallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
